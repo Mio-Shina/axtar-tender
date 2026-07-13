38af00a5fe4e5105c45419fa13fb00289e5801f8
Kibar Jafarguliyev <jafarguliyev.kibaret@gmail.com>
2026-07-13 13:01:41 +0400

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,3 @@ password = os.environ.get("MYSQLPASSWORD")
 host = os.environ.get("MYSQLHOST")
 port = int(os.environ.get("MYSQLPORT", 3306))
 db_name = os.environ.get("MYSQLDATABASE")
-
-print("DB Host:", host)
-print("DB User:", user)
-print("DB Name:", db_name)
